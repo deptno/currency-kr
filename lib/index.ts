82745a40043e3d154f549618861dc811a8aff7d7
Bonggyun Lee <deptno@gmail.com>
2017-03-20 02:42:56 +0900

    test definition

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,11 @@
 import * as currency from 'currency-formatter';
+import {KRW} from '../index';
 
 const UNIT = ['', '십', '백', '천', '만', '십만', '백만', '천만', '억', '십억', '백억', '천억', '조', '십조', '백조', '천조'];
-const UNIT_EN = ['w', 'w10', 'w100', 'w1000', 'm', 'm10', 'm100', 'm1000', 'b', 'b10', 'b100', 'b1000', 't', 't10', 't100', 't1000']
+const UNIT_EN = ['w', 'w10', 'w100', 'w1000', 'm', 'm10', 'm100', 'm1000', 'b', 'b10', 'b100', 'b1000', 't', 't10', 't100', 't1000'];
 const comma = (unit = 1) => (raw: number, precision = 0) => currency.format(raw / unit, {precision});
 
-export default UNIT.reduce((ret, cur, index) => {
+export default UNIT.reduce<KRW>((ret, cur, index) => {
     ret[UNIT_EN[index]] = ret[`${cur}원`] = comma((Math.pow(10, index)));
     return ret;
-}, {});
+}, {} as KRW);
