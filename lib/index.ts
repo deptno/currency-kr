7edc8adb82d99b943ff1aa8485eab9adcd0c52b0
Bonggyun Lee <deptno@gmail.com>
2017-10-21 22:37:14 +0900

    fix type missmatching

--- lib/index.ts
+++ lib/index.ts
@@ -4,7 +4,9 @@ const UNIT = ['', '십', '백', '천', '만', '십만', '백만', '천만', '억
 const UNIT_EN = ['w', 'w10', 'w100', 'w1000', 'm', 'm10', 'm100', 'm1000', 'b', 'b10', 'b100', 'b1000', 't', 't10', 't100', 't1000'];
 const comma = (unit = 1) => (raw: number, precision = 0) => currency.format(raw / unit, {precision});
 
-export = UNIT.reduce((ret, cur, index) => {
+declare var module
+
+module.exports = UNIT.reduce((ret, cur, index) => {
     ret[UNIT_EN[index]] = ret[`${cur}원`] = comma((Math.pow(10, index)));
     return ret;
 }, {});
